62e32f97a8ce6f34bbe09b214a17c2f11aa42248
vscorpio <victor.puiu01@gmail.com>
2018-03-31 14:02:30 +0300

    various fixes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 import os
 import random
 import string
+import time
 import smtplib
 from time import sleep
 from subprocess import Popen, PIPE, STDOUT
@@ -33,27 +34,31 @@ public_ip_address = requests.get('http://ip.42.pl/raw').text
 def send_mail(): 
 	server = smtplib.SMTP('SMTP_ADDRESS', SMTP_PORT)
 	server.starttls()
-	server.login("SMTP_USERNAME", "SMTP_PASSWORD")
- 
+	server.login("SMTP_USERNAME", "SMTP_PASSWORD") 
 	msg = ("This message is sent from DNSStatus running on {}\nThe target host ( {} ) has been reported down and not reachable.\nDo not reply to this message!\nHASH:{}".format(public_ip_address, monitoring_ip, id_generator()))
 	server.sendmail("SMTP_USERNAME", "SMTP_DESTINATION_ADDRESS", msg)
 	server.quit()
 
 
-
-
-monitoring_ip = "REMOTE HOST IP"
+mailsthissession = 1
+monitoring_ip = "93.113.134.243"
 #script
 
+global startdate
+startdate = time.strftime("%d/%m/%Y at %H:%M:%S")
 
 print("=======================")
 sleep(0.3)
 print("====DNSStatus v0.1a====")
+print("ONLY FOR USE WITH LINUX OSs!")
+print("ONLY FOR USE WITH LINUX OSs!")
+print("ONLY FOR USE WITH LINUX OSs!")
 sleep(0.3)
 print("=======================")
 sleep(0.3)
 
 print("DNSStatus initializing...")
+print(startdate)
 sleep(0.3)
 print("Setting lang to: EN_US...OK")
 sleep(0.3)
@@ -77,19 +82,18 @@ print("Monitoring PORT(s) set to: 80 [future versions multiple ports]")
 server_ip = monitoring_ip
 
 
+
 host_up = True if os.system("ping -c 1 " + monitoring_ip) is 0 else False
 
 var = 1
 while var==1:
+	global mailsthissession
 	if host_up is True:
-		print('Target host is UP. Sleeping for 2 hours!')
+		print('Target host is UP. Sleeping for 5 minutes...')
 	else:
-		print('Target host is DOWN. Warning! Sending MAIL...rechecking in 2 hours...')
 		send_mail()
-	sleep(1800)
-	print('1h30mins left to recheck...')
-	sleep(1800)
-	print('1h left to recheck...')
-	sleep(3600)
-	print('rechecking...')
+		print('Target host is DOWN. Warning! Sending MAIL...sent mail #{}...'.format(mailsthissession))
+		mailsthissession+= 1
+	sleep(300)
+	host_up = True if os.system("ping -c 1 " + monitoring_ip) is 0 else False
 
